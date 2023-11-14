6e0f407a1a6586bc0f45099b43a49974638a1069
Charryl Subrio <charrylsubrio@gmail.com>
2023-11-14 17:54:03 +0800

    Add files via upload
    Added acceptLetter.c

---
+++ acceptLetter.c
@@ -0,0 +1,28 @@
+/*
+CHARRYL SUBRIO
+
+This program continuously accepts a letter from the user and displays it until the user wishes to stop.
+
+November 14, 2023
+*/
+
+#include <stdio.h>
+
+int main() {
+	char ans = 'Y', letter;
+
+	while(toupper(ans) == 'Y') {
+		printf("Enter letter: ");
+		scanf("%c", &letter);
+		fflush(stdin);
+		
+		printf("Inputted: %c", letter);
+		
+		printf("\n\nInput Y to continue: ");
+		ans = getche();
+		
+		printf("\n\n");
+	}
+
+	return 0;
+}
