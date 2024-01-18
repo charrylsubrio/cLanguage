2d23f060eaf171ed49d01e08c75b55d7e8a3dbbf
Charryl Subrio <charrylsubrio@gmail.com>
2024-01-18 16:16:19 +0800

    Add files via upload

---
+++ conditionStatement.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main() {
+	int num1;
+	printf("Enter a number: ");
+	scanf("%d", &num1);
+	if(num1 > 0) {
+		printf("Positive");
+	} else if(num1 < 0) {
+		printf("Negative");
+	} else {
+		printf("Zero");
+	}
+}
