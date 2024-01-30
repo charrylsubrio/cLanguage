27c83d95e56f17661748f3d856deefb38a48f276
Charryl Subrio <charrylsubrio@gmail.com>
2024-01-29 22:38:08 -0800

    Add files via upload

---
+++ validate1.c
@@ -0,0 +1,23 @@
+/*
+	14)	Write a program in C program that validates the entry of the user.
+	It should accept positive numbers only.  If the user inputs a negative or 0, it should ask the user to input again.
+	The program should display the inputted positive number.
+*/
+
+#include <stdio.h>
+
+main() {
+	int num1;
+	
+	do {
+		printf("Enter any positive number: ");
+		scanf("%d", &num1);
+		
+		if(num1 <= 0) {
+			printf("\nPlease input a positive number only\n\n");
+		}
+	} while (num1 <= 0);
+	
+	printf("\n%d", num1);
+}
+
