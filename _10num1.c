8f24e91fb35cf14de2333489db060d00b80da175
Charryl Subrio <charrylsubrio@gmail.com>
2024-01-29 21:03:05 -0800

    Add files via upload

---
+++ _10num1.c
@@ -0,0 +1,16 @@
+/*
+	11)	Using the looping logic, write a program in C to display the first 10 natural numbers. 
+	Expected Output: 1 2 3 4 5 6 7 8 9 10
+*/
+
+#include <stdio.h>
+
+main() {
+	int i;
+	
+	for(i = 1; i <= 10; i++) {
+		printf("%d", i);
+	}
+}
+
+
