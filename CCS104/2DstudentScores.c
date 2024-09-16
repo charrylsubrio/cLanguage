2985ff5e2b67e1b48060e7e5777c6eedcaafc190
Charryl Subrio <charrylsubrio@gmail.com>
2024-09-16 14:01:24 +0800

    Add files via upload

---
+++ CCS104/2DstudentScores.c
@@ -0,0 +1,29 @@
+/*
+Write a program that accepts from the user the scores of 3 students in the 4 quizzes and stores these scores in an array. Display the scores of each student.
+It should also do the following tasks:
+ a. Ask the user to choose whose scores to display (student 1, student 2 or student 3).  
+ b. Ask the user to choose a student number, and computes and displays the average score of that student.
+ c. Ask the user to choose a quiz number (columns: 1, 2, 3 or 4), it finds the highest score, finds lowest score,  and computes and displays the average score of the specific quiz.
+
+Your program should be menu-driven and includes the following options:
+ 1. Display all scores
+ 2. Display the scores of a specific student
+ 3. Display the average score of a specific student
+ 4. Display quiz statistics (Find the lowest score, highest score, average score) 
+
+Note:  Make sure to include functions in your program, not just the main function.  
+
+You may consider the following functions:
+ a. acceptScores() - accept scores of 3 students in the 4 quizzes and store these into an array
+ b. displayStudentScore() - displays the scores of a specific student. It allows the user to input the student number.
+ c. displayAllScores() - displays all the scores of all the students
+ d. computeStudentAve() - calculates the average score of a specific student.  It allows the user to input the student number.
+ e. showQuizStats() - should compute the lowest, highest and average score of a specific quiz.  It allows the user to input the quiz number.
+*/
+
+#include <stdio.h>
+
+int main() {
+	
+	return 0;
+}
